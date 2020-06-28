65830037ae4d1eed5bd85b3e1c9ca0d794652a48
r007@68 <53935554+solopsy@users.noreply.github.com>
2020-06-28 12:04:53 +0000

    Add files via upload

---
+++ TXB.sh
@@ -0,0 +1,632 @@
+#!/bin/bash
+echo -e '\e[0m'
+clear
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo  '
+		█   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo""
+echo""
+echo""
+echo""
+echo -e '               /█1%...................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "  "
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /█3%....................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /█6%......................./'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /██10%....................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /██12%....................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /███15%..................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '               /█████19%..................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '               /██████26%..................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /████████30%................../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /██████████45%................/'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /█████████████50%............./'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /████████████████60%........../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /███████████████████88%......./'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo  '
+                █   ▄▀▄ ▄▀▄ █▀▄ █ █▄ █ ▄▀
+                █▄▄ ▀▄▀ █▀█ █▄▀ █ █ ▀█ ▀▄█
+'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '                /██████████████████████95%..../'
+sleep 0.1
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e '\e[38;5;56m'
+echo '
+    ░▀▀█▀▀░░░░█▀▄▀█░█░▒█░█░█░░▒█▀▀▄░█▀▀▄░█▀▀▄░█▀▀▄░█▀▀░█▀▀▄
+    ░░▒█░░░▀▀░█░▀░█░█░▒█░▄▀▄░░▒█▀▀▄░█▄▄█░█░▒█░█░▒█░█▀▀░█▄▄▀
+    ░░▒█░░░░░░▀░░▒▀░░▀▀▀░▀░▀░░▒█▄▄█░▀░░▀░▀░░▀░▀░░▀░▀▀▀░▀░▀▀
+
+
+'
+echo -e '      \e[0m         [██████████████████████ 100%]'
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1.8
+clear
+echo '
+
+	█▀▀██▀▀█
+	   ██          ▄▄ ▄▄ ▄▄   ▄▄▄ ▄▄▄  ▄▄▄ ▄▄▄
+	   ██           ██ ██ ██   ██  ██   ▀█▄▄▀
+	   ██     ████  ██ ██ ██   ██  ██    ▄█▄
+	  ▄██▄         ▄██ ██ ██▄  ▀█▄▄▀█▄ ▄█  ██▄
+
+	▀██▀▀█▄
+	 ██   ██   ▄▄▄▄   ▄▄ ▄▄▄   ▄▄ ▄▄▄     ▄▄▄▄  ▄▄▄ ▄▄
+	 ██▀▀▀█▄  ▀▀ ▄██   ██  ██   ██  ██  ▄█▄▄▄██  ██▀ ▀▀
+	 ██    ██ ▄█▀ ██   ██  ██   ██  ██  ██       ██
+	▄██▄▄▄█▀  ▀█▄▄▀█▀ ▄██▄ ██▄ ▄██▄ ██▄  ▀█▄▄▄▀ ▄██▄ ' | lolcat
+echo ""
+
+echo -e '		\e[92m✩░▒▓▆▅▃▂▁ \e[38;5;11mcode by r007@68 \e[92m▁▂▃▅▆▓▒░✩'
+
+echo ""
+echo ""
+
+
+echo -e '       \e[38;5;14mChoose any banner'
+
+echo -e '      \e[38;5;82m~~~~~~~~~~~~~~~~~~~'
+
+echo ""
+
+echo -e '  \e[38;5;196m1   \e[38;5;49mAlien       '
+echo ""
+echo -e '  \e[38;5;196m2   \e[38;5;49mBat         '
+echo ""
+echo -e '  \e[38;5;196m3   \e[38;5;49mBiohazard   '
+echo ""
+echo -e '  \e[38;5;196m4   \e[38;5;49mDragon      '
+echo ""
+echo -e '  \e[38;5;196m5   \e[38;5;49mEvil eye    '
+echo ""
+echo -e '  \e[38;5;196m6   \e[38;5;49mGost Buster '
+echo ""
+echo -e '  \e[38;5;196m7   \e[38;5;49mGrim Reaper '
+echo ""
+echo -e '  \e[38;5;196m8   \e[38;5;49mKeyboard    '
+echo ""
+echo -e '  \e[38;5;196m9   \e[38;5;49mLinux       '
+echo ""
+echo -e '  \e[38;5;196m10  \e[38;5;49mLinon        '
+echo ""
+echo -e '  \e[38;5;196m11  \e[38;5;49mMinotaur    '
+echo ""
+echo -e '  \e[38;5;196m12  \e[38;5;49mRedhat      '
+echo ""
+echo -e '  \e[38;5;196m13  \e[38;5;49mSkull       '
+echo ""
+echo -e '  \e[38;5;196m14  \e[38;5;49mSnail       '
+echo ""
+echo -e '  \e[38;5;196m15  \e[38;5;49mWeed        '
+echo ""
+echo -e '  \e[38;5;196m16  \e[38;5;49myin yang    '
+echo ""
+echo -e '  \e[38;5;196m17  \e[38;5;49mText Banner   '
+echo ""
+
+echo -e '  \e[0m99 \e[38;5;14mFor Exit'
+
+echo ""
+
+echo ""
+
+echo -e '\e[38;5;10m'
+
+read -p ' choose any option : ' input
+
+chmod +x /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+rm /data/data/com.termux/files/usr/etc/motd
+if [ $input -eq 1 ] ; then
+
+        clear
+
+	chmod +x /data/data/com.termux/files/home/T-mux-Banner/db/cod/alien.sh
+
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/alien.sh
+
+elif [ $input -eq 2 ] ; then
+
+        clear
+
+	chmod +x /data/data/com.termux/files/home/T-mux-Banner/db/cod/bat.sh
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/bat.sh
+
+elif [ $input -eq 3 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Biohazard
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 4 ] ; then
+
+        clear
+
+	chmod +x /data/data/com.termux/files/home/T-mux-Banner/db/cod/dragon.sh
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/dragon.sh
+
+elif [ $input -eq 5 ] ; then
+
+        chmod +x /data/data/com.termux/files/home/T-mux-Banner/db/cod/evil.sh
+
+
+        bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/evil.sh
+
+elif [ $input -eq 6 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Gostbuster
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 7 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Grimreaper
+
+        bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 8 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Keyboard
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 9 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Linux
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 10 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Lion
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 11 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Minotaur
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 12 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Redhat
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 13 ] ; then
+
+        clear
+
+	chmod +x /data/data/com.termux/files/home/T-mux-Banner/db/cod/skull.sh
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/skull.sh
+
+elif [ $input -eq 14 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Snail
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 15 ] ; then
+
+        clear
+
+	cd /data/data/com.termux/files/home/T-mux-Banner/db/bashrc/Weed
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/main.sh
+
+elif [ $input -eq 16 ] ; then
+
+        clear
+
+	chmod +x /data/data/com.termux/files/home/T-mux-Banner/db/cod/yingyang.sh
+
+	bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/yingyang.sh
+
+elif [ $input -eq 17 ] ; then
+
+        clear
+
+        chmod +x /data/data/com.termux/files/home/T-mux-Banner/db/cod/fig.sh
+
+        bash /data/data/com.termux/files/home/T-mux-Banner/db/cod/fig.sh
+
+elif [ $input -eq 99 ] ; then
+
+        clear
+
+        echo '
+        ███                     ██
+         █      █████ █ █ █ █   █ █ ███ ███ ███ ███ ███
+         █  ███ █ █ █ █ █  █    ██    █ █ █ █ █ ███ █
+         █      █ █ █ █ █  █    █ █ ███ █ █ █ █ █   █
+         █      █ █ █ ███ █ █   ██  ███ █ █ █ █ ███ █ ' | lolcat
+
+        echo ""
+
+	sleep 2
+
+        exit 0
+else
+        clear
+
+        echo '
+                                █
+                                               █
+        █ █ █ ███ ███ ███ ███   █ ███ ███ █ █ ███
+        █ █ █ █   █ █ █ █ █ █   █ █ █ █ █ █ █  █
+        █ █ █ █   █ █ █ █ █ █   █ █ █ █ █ █ █  █
+         █ █  █   ███ █ █ ███   █ █ █ ███ ███  ██
+                            █         █
+                          ███         █ ' | lolcat
+
+        sleep 2
+
+        clear
+
+        bash TXB.sh
+
+
+
+fi
